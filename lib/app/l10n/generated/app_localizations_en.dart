9467f0fa441ef02ed50851ab2e0724191e64fba0
juncook <nguyenngoctuyen188@gmail.com>
2025-11-17 22:42:48 +0700

    gen locale

---
+++ lib/app/l10n/generated/app_localizations_en.dart
@@ -0,0 +1,20 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get txt_hello => 'Hello';
+
+  @override
+  String get txt_ready_to_explore_beyond_boundaries =>
+      'Ready to explore beyond boundaries?';
+
+  @override
+  String get txt_your_journey_starts_here => 'Your Journey Starts Here';
+}
---
+++ lib/app/l10n/generated/app_localizations_vi.dart
@@ -0,0 +1,21 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Vietnamese (`vi`).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get txt_hello => 'Xin Chào';
+
+  @override
+  String get txt_ready_to_explore_beyond_boundaries =>
+      'Sẵn sàng khám phá vượt ra ngoài ranh giới?';
+
+  @override
+  String get txt_your_journey_starts_here =>
+      'Hành trình của bạn bắt đầu từ đây';
+}
